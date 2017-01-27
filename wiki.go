19c77fcaa9f078c1d4b1fa0418e5aba32577e878
David Walsh <dawalsh@gmail.com>
2017-01-26 22:31:37 -0500

    Implement editing

--- wiki.go
+++ wiki.go
@@ -23,17 +23,29 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit"):]
+	title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -47,9 +59,7 @@ func loadPage(title string) (*Page, error) {
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
-http:
-	HandleFunc("/edit/", editHandler)
-http:
-	HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
